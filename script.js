6ca6fce65ae1ae734be9ba656fbc19352917b52a
Zach Stenger <stengezm@mail.uc.edu>
2023-02-06 12:26:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const milesToKilometers = miles => {
   const km = miles * 1.60934
-  const result = `The distance of ${km.toFixed(0)} kms is equal to ${miles} miles`
+  const result = `The distance of ${km} kms is equal to ${miles} miles`
   return result
 }
 
 const result = milesToKilometers(209.2142)
 console.log(result)
-alert(result)
